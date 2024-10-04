504856db707604c7707f6a881e934d80cffa96a1
vovksim <ivan.mykhailov11@gmail.com>
2024-10-04 18:29:18 +0300

    Created abstract class for Data Access Object which holds all the necessary methods.

---
+++ src/main/java/com/musicsys/DAO/AbstractDAO.java
@@ -0,0 +1,65 @@
+package com.musicsys.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.sql.ResultSet;
+
+public abstract class AbstractDAO<T> implements AutoCloseable {
+  Connection connection;
+
+  public interface ParameterMapper<T> {
+    public void mapInsertParameters(PreparedStatement stmt, T entity) throws SQLException;
+    public default void mapUpdateParameters(PreparedStatement stmt, T entity) throws SQLException {};
+    public void mapIdParameter(PreparedStatement stmt, Integer id) throws SQLException;
+  }
+
+  public interface ResultSetParser<T> {
+    public ArrayList<T> parseInList(ResultSet rs) throws SQLException;
+  }
+
+  protected final ParameterMapper<T> parameterMapper;
+  protected final ResultSetParser<T> resultSetParser;
+
+
+  protected AbstractDAO(Connection _connection, ParameterMapper<T> _mapper, ResultSetParser<T> _parser) {
+    this.connection = _connection;
+    this.parameterMapper = _mapper;
+    this.resultSetParser = _parser;
+  }
+
+  protected void insert(T entity, String query) throws SQLException {
+    PreparedStatement stmt = connection.prepareStatement(query);
+    parameterMapper.mapInsertParameters(stmt, entity);
+    stmt.executeUpdate();
+  }
+
+  protected void update(T entity, String query) throws SQLException {
+    PreparedStatement stmt = connection.prepareStatement(query);
+    parameterMapper.mapUpdateParameters(stmt, entity);
+    stmt.executeUpdate();
+  }
+
+  protected void delete(Integer id, String query) throws SQLException {
+    PreparedStatement stmt = connection.prepareStatement(query);
+    parameterMapper.mapIdParameter(stmt, id);
+    stmt.executeUpdate();
+  }
+
+  protected T getById(Integer id, String query) throws SQLException {
+    PreparedStatement stmt = connection.prepareStatement(query);
+    parameterMapper.mapIdParameter(stmt, id);
+    return resultSetParser.parseInList(stmt.executeQuery()).get(0);
+  }
+
+  protected ArrayList<T> getAll(String query) throws SQLException {
+    PreparedStatement stmt = connection.prepareStatement(query);
+    return resultSetParser.parseInList(stmt.executeQuery());
+  }
+
+  @Override
+  public void close() throws SQLException {
+    connection.close();
+  }
+}
