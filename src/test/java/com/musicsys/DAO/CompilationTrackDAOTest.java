9fffd9cc8dd8aca545ccb4e9f8aff2b277a96ec1
vovksim <ivan.mykhailov11@gmail.com>
2024-10-05 18:19:28 +0300

    Initial commit.

---
+++ src/test/java/com/musicsys/DAO/CompilationTrackDAOTest.java
@@ -0,0 +1,40 @@
+package com.musicsys.DAO;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompilationTrackDAOTest {
+
+    @Test
+    void getAllRelationsById() {
+    }
+
+    @Test
+    void removeRelation() {
+    }
+
+    @Test
+    void deleteRelation() {
+    }
+
+    @Test
+    void deleteAllTracksFromCompilation() {
+    }
+
+    @Test
+    void deleteTrackFromAllCompilations() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void getAllRelationsForTrack() {
+    }
+
+    @Test
+    void getAllRelationsForCompilation() {
+    }
+}
