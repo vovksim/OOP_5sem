d154be7a01d0a232f71d6d036e868bcb3501d060
vovksim <ivan.mykhailov11@gmail.com>
2024-10-05 17:33:02 +0300

    Improved indentation. Implemented equals() method.

--- src/main/java/com/musicsys/Entity/CompilationTrack.java
+++ src/main/java/com/musicsys/Entity/CompilationTrack.java
@@ -1,20 +1,28 @@
 package com.musicsys.Entity;
 
 public class CompilationTrack {
-  Integer trackId;
-  Integer compilationId;
+    Integer trackId;
+    Integer compilationId;
 
 
-  public CompilationTrack(Integer _compilationId, Integer _trackId) {
-    this.compilationId = _compilationId;
-    this.trackId = _trackId;
-  }
+    public CompilationTrack(Integer _compilationId, Integer _trackId) {
+        this.compilationId = _compilationId;
+        this.trackId = _trackId;
+    }
 
-  public Integer getTrackId() {
-    return compilationId;
-  }
+    public Integer getTrackId() {
+        return compilationId;
+    }
 
-  public Integer getCompilationId() {
-    return compilationId;
-  }
+    public Integer getCompilationId() {
+        return compilationId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof CompilationTrack track) {
+            return track.compilationId.equals(track.getTrackId()) && track.trackId.equals(track.getTrackId());
+        }
+        return false;
+    }
 }
