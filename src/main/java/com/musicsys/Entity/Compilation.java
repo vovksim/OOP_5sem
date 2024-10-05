736eacb65722221e3bb6f5a4d0fd58aab797b465
vovksim <ivan.mykhailov11@gmail.com>
2024-10-05 20:10:45 +0300

    Fixed indentation. Defined equals(). Added getter for id.

--- src/main/java/com/musicsys/Entity/Compilation.java
+++ src/main/java/com/musicsys/Entity/Compilation.java
@@ -1,21 +1,33 @@
 package com.musicsys.Entity;
 
 public class Compilation {
-  Integer id;
-  String compilationTitle;
+    Integer id;
+    String compilationTitle;
 
-  public Compilation(Integer _id, String _compilationTitle) {
-    this.id =_id;
-    this.compilationTitle = _compilationTitle;
-  }
+    public Compilation(Integer _id, String _compilationTitle) {
+        this.id = _id;
+        this.compilationTitle = _compilationTitle;
+    }
 
-  public void setCompilationTitle(String newTitle) {
-    this.compilationTitle = newTitle;
-  }
+    public void setCompilationTitle(String newTitle) {
+        this.compilationTitle = newTitle;
+    }
 
-  public String getCompilationTitle() {
-    return compilationTitle;
-  }
+    public String getCompilationTitle() {
+        return compilationTitle;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Compilation comp) {
+            return this.id.equals(comp.getId()) && this.compilationTitle.equals(comp.getCompilationTitle());
+        }
+        return false;
+    }
 }
 
 
