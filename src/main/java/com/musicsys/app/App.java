5005cb4f953ab28995a197c2fe5a09d266109b86
vovksim <ivan.mykhailov11@gmail.com>
2024-10-15 09:27:10 +0300

    Simple demo-run.

--- src/main/java/com/musicsys/app/App.java
+++ src/main/java/com/musicsys/app/App.java
@@ -1,10 +1,25 @@
 package com.musicsys.app;
 
-/**
- * Hello world!
- */
+
+import com.musicsys.Entity.Compilation;
+import com.musicsys.Entity.CompilationTrack;
+import com.musicsys.Entity.Track;
+import com.musicsys.MusicService.MusicService;
+
+import java.time.Duration;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            MusicService musicService = new MusicService();
+            musicService.addTrack(new Track(1, "Track1", "rock", Duration.ofSeconds(100)));
+            musicService.addTrack(new Track(2, "Track2", "rap", Duration.ofSeconds(120)));
+
+            musicService.addCompilation(new Compilation(1, "Compilation1"));
+            musicService.addTrackToCompilation(new CompilationTrack(1, 1));
+            System.out.println(musicService.getCompilationDTO(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
